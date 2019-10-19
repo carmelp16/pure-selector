325ec8c732c57129b72b6de3275fbe3c28e2ecb1
Carmel Doe <16carmel@gmail.com>
2019-10-19 16:03:23 +0300

    Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+export default {
+  mode: "production",
+
+  resolve: {
+    extensions: [".ts"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      }
+    ]
+  }
+};
